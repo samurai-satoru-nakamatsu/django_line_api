b3a20ed26225b7cd2c4b6a27753d181e1bef1bf7
nakamatsu <nakamatsu@hands-inc.co.jp>
2022-08-20 20:00:53 +0900

    modify admin

--- webhook/admin.py
+++ webhook/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from webhook.models import WebhookEventObject
 
 
-class WebhookEventObjectAdmin(admin.ModelAdmin):
-    fields = ['id', 'created_at']
-
-
-admin.site.register(WebhookEventObject, WebhookEventObjectAdmin)
+admin.site.register(WebhookEventObject)
